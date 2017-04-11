17c1c9d36760f2dd6daf5244b4c0f3a30112bf5f
nesta533 <nesta533@hotmail.com>
2017-04-11 18:04:37 +0800

    stable

--- GenerateFileWithFixedSize/Program.cs
+++ GenerateFileWithFixedSize/Program.cs
@@ -18,24 +18,53 @@ namespace GenerateFileWithFixedSize
             app.HelpOption("--help|-h|-?");
             app.ShowHelp("usage:GenerateLargeFile -o file -s size");
             var optFile = app.Option("--output -o <value>", "file path", CommandOptionType.SingleValue);
-            var optSize = app.Option("--size -s <value>", "file size", CommandOptionType.SingleValue);
-
+            var optSize = app.Option("--size -s <value>", "file size in K units", CommandOptionType.SingleValue);
             Func<CommandOption, CommandOption, int> onAppExecute = ((file, size) =>
             {
+                if (!file.HasValue())
+                {
+                    Console.Error.WriteLine($"The file path is null.");
+                    return -1;
+                }
 
+                string targetFile = file.Value();
+                if (string.IsNullOrEmpty(targetFile))
+                {
+                    Console.Error.WriteLine($"The file path is invalid.");
+                    return -2;
+                }
+
+                if (!size.HasValue())
+                {
+                    Console.Error.WriteLine($"The size is null.");
+                    return -1;
+                }
+
+                int fixedSize = 0;
+                if (!int.TryParse(size.Value(), out fixedSize))
+                {
+                    Console.Error.WriteLine($"Invalid size.");
+                    return -3;
+                }
+
+                const int kunit = 1024;
+                fixedSize = fixedSize * kunit;
+
+                using (FileStream fs = new FileStream(targetFile, FileMode.Create))
+                {
+                    fs.Seek(fixedSize, SeekOrigin.Begin);
+                    byte[] myByteArray = Enumerable.Repeat((byte)'a', fixedSize).ToArray();
+                    fs.Seek(0, SeekOrigin.Begin);
+                    fs.Write(myByteArray, 0, fixedSize);
+                }
+
+                Console.WriteLine($"Successful written!");
                 return 0;
             });
 
+            app.OnExecute(() => { return onAppExecute(optFile, optSize); });
+
             app.Execute(args);
-
-
-            const int fixedSize = 100 * 1024; //10 M
-            FileStream fs = new FileStream(@"c:\temp\huge_dummy_file.txt", FileMode.Create);
-            fs.Seek(fixedSize, SeekOrigin.Begin);
-            byte[] myByteArray = Enumerable.Repeat((byte)'a', fixedSize).ToArray();
-            fs.Seek(0, SeekOrigin.Begin);
-            fs.Write(myByteArray, 0, fixedSize);
-            fs.Close();
 
         }
     }
